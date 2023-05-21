5e0c566d208aa94ea3a1ffa711343cc3c24b7096
Om Singh <94895043+omsinghthakur@users.noreply.github.com>
2023-05-21 10:49:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-function threeSum(arr, target) {
-// write your code here
-  
+function decimalToBinary(decimal) {
+  // Base case: if the decimal number is 0, return '0'
+  if (decimal === 0) {
+    return '0';
+  }
+
+  let binary = ''; // Variable to store the binary representation
+
+  // Convert decimal to binary
+  while (decimal > 0) {
+    // Get the remainder when dividing by 2 (either 0 or 1)
+    const remainder = decimal % 2;
+    // Add the remainder to the binary representation
+    binary = remainder + binary;
+    // Integer division by 2 to move to the next bit
+    decimal = Math.floor(decimal / 2);
+  }
+
+  return binary;
 }
 
-module.exports = threeSum;
+// Example usage:
+const decimalNumber = 10;
+const binaryNumber = decimalToBinary(decimalNumber);
+console.log(binaryNumber); // Output: 1010
