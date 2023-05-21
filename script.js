a7f4e31b85528b7703ffdfe04102034536e32b82
Om Singh <94895043+omsinghthakur@users.noreply.github.com>
2023-05-21 14:51:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,22 +2,14 @@ function decimalToBinary(decimal) {
   // Base case: if the decimal number is 0, return '0'
   if (decimal === 0) {
     return '0';
-  }
-
-  let binary = ''; // Variable to store the binary representation
-
-  // Convert decimal to binary
+  }   
+  let binary = ''; 
   while (decimal > 0) {
-    // Get the remainder when dividing by 2 (either 0 or 1)
-    const remainder = decimal % 2;
-    // Add the remainder to the binary representation
-    binary = remainder + binary;
-    // Integer division by 2 to move to the next bit
+    binary = (decimal % 2)+binary; 
     decimal = Math.floor(decimal / 2);
-  } 
-  return binary;
-} 
-
-const decimalinput=parseint(prompt("Enter a decimal number:"));
-const binryOutput=decimalToBinary(decimalinput);
-console.log("Binary equivalent:",binaryOutput); 
+  }  
+  return binary; 
+}   
+const decimalinput=parseInt(prompt("Enter a decimal number:")); 
+const binaryOutput=decimalToBinary(decimalinput);
+console.log("Binary equivalent:",binaryOutput);  
