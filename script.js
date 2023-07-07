24f23abdcb10a412b9908453162699e17eb3dfe0
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2023-07-07 15:14:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
-function decimalToBinary(decimal) {
-  // Base case: if the decimal number is 0, return '0'
-  if (decimal === 0) {
-    return '0';
-  }   
-  let binary = ''; 
-  while (decimal > 0) {
-    binary = (decimal % 2)+binary; 
-    decimal = Math.floor(decimal / 2);
-  }  
-  return binary; 
-}   
-const decimalinput=parseInt(prompt("Enter a decimal number:")); 
-const binaryOutput=decimalToBinary(decimalinput);
-console.log("Binary equivalent:",binaryOutput);  
+function decimalToBinary(num) {
+  // Write your code here
+}
+
+window.decimalToBinary = decimalToBinary;
